7b02395616f8c24a4a30608650a48e9f7eb7242a
Sulaiman Desktop <msulaimanmisri@gmail.com>
2024-08-11 20:49:35 +0800

    Update Code

--- src/App/StringConvert.php
+++ src/App/StringConvert.php
@@ -4,8 +4,28 @@ namespace SulaimanMisri\App;
 
 class StringConvert
 {
-    public static function kasiBesar(string $string): string
+    public static function kasiBesar(string $string)
     {
         return strtoupper($string);
+    }
+
+    public static function kasiKecil(string $string)
+    {
+        return strtolower($string);
+    }
+
+    public static function firstLetterJeBesar(string $string)
+    {
+        return ucwords($string);
+    }
+
+    public static function tukarKeSlug(string $string)
+    {
+        return str_replace(' ', '-', $string);
+    }
+
+    public static function buangExtraSpace(string $string)
+    {
+        return preg_replace('/\s+/', ' ', trim($string));
     }
 }
